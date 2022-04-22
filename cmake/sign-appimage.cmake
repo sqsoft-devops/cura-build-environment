d280bcc299f1980d94cdd70209dba5cd852afde2
e1479abfe2 <urosa9@gmail.com>
2022-04-22 14:27:49 +0200

    add Jenkinsfile.linux and bash script to run in jenkins

--- cmake/sign-appimage.cmake
+++ cmake/sign-appimage.cmake
@@ -1,9 +1,9 @@
 # Copyright (c) 2022 Ultimaker B.V.
 # Cura's build system is released under the terms of the AGPLv3 or higher.
 
-add_custom_target(signing ALL COMMENT "Create the asc file for the AppImage file.")
+add_custom_target(signing ALL COMMENT "Create the sha256sum file for the AppImage file.")
 add_custom_command(
         TARGET
             signing
-        COMMAND sha256sum ${installer_DIR}/dist/${INSTALLER_FILENAME} >> ${installer_DIR}/dist/${INSTALLER_BASE_FILENAME}.asc)
+        COMMAND sha256sum ${installer_DIR}/dist/${INSTALLER_FILENAME} >> ${installer_DIR}/dist/${INSTALLER_BASE_FILENAME}.sha256sum)
 add_dependencies(signing packaging)
