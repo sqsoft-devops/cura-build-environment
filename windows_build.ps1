0c05736ddea661781a376a0237718e9bf48d2aad
sqsoft-devops <l.rosa@soniqsoft.pl>
2022-05-20 13:23:19 +0200

    Update windows_build.ps1

--- windows_build.ps1
+++ windows_build.ps1
@@ -2,5 +2,5 @@ mkdir installation
 mkdir build
 cd build 
 cmd /k "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvars64.bat"
-#cmake -G "NMake Makefiles" -DCMAKE_PREFIX_PATH="C:\jenkins\workspace\cura-pipeline-windows-staging-5.0\installation" -DCMAKE_INSTALL_PREFIX="C:\jenkins\workspace\cura-pipeline-windows-staging-5.0\installation" ..
+cmake -DCMAKE_PREFIX_PATH="C:\jenkins\workspace\cura-pipeline-windows-staging-5.0\installation" -DCMAKE_INSTALL_PREFIX="C:\jenkins\workspace\cura-pipeline-windows-staging-5.0\installation" ..
 #cmake --build .
