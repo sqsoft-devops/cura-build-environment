c0719c82431ab04fa1ae7d53be9170350abb6d49
sqsoft-devops <l.rosa@soniqsoft.pl>
2022-05-20 11:49:39 +0200

    Update windows_build.ps1

--- windows_build.ps1
+++ windows_build.ps1
@@ -2,5 +2,5 @@ mkdir installation
 mkdir build
 cd build 
 cmd /k "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvars64.bat"
-cmake -G "NMake Makefiles" -DCMAKE_PREFIX_PATH=../installation -DCMAKE_INSTALL_PREFIX=../installation -DCMAKE_BUILD_TYPE=Release ..
+cmake -G "NMake Makefiles" -DCMAKE_PREFIX_PATH="../installation" -DCMAKE_INSTALL_PREFIX="../installation" -DCMAKE_BUILD_TYPE=Release ..
 cmake --build .
